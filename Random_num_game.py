9bc7e6bb8636cae38074ee95686d0959759b9535
younesvatan78 <62533356+younesvatan78@users.noreply.github.com>
2020-03-23 09:39:12 +0430

    Add files via upload

---
+++ Random_num_game.py
@@ -0,0 +1,22 @@
+import random
+
+takenNum = random.randrange(1000)
+converted_input = int(input("Try to guess the number: "))
+print(takenNum)
+
+while True:   
+    if converted_input == takenNum:
+        print('yay,Hit! (:')
+        again = input('try again? ')
+        if again == 'y':
+            converted_input = int(input("Try to guess the number: "))
+        if again != 'y':
+            print('bye')
+            break
+    elif converted_input > takenNum:
+        print('nooo, its smaller')
+        converted_input = int(input("Try to guess the number: "))
+    else:
+        print('nooo, man try again a bigger one (;')
+        converted_input = int(input("Try to guess the number: "))
+       
